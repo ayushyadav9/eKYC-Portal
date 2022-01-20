57a8c856d92be10dc039444dafca32f1b4d0fdcb
Ayush Yadav <ayushyadav935@gmail.com>
2022-01-20 13:27:32 +0530

    Outside src error

--- client/src/components/utils/web3.js
+++ client/src/components/utils/web3.js
@@ -1,5 +1,5 @@
 import Web3 from "web3";
-import {abi,networks} from "../../../../blockchain/build/contracts/KYC.json";
+import {abi,networks} from "./KYC.json";
 
 async function InitialiseWeb3() {
   await window.ethereum.enable();
