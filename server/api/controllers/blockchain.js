4138d953b8b3d42b119887437a72b20b1b3782c4
soul0101 <53980340+soul0101@users.noreply.github.com>
2022-01-20 17:02:12 +0530

    Client side integration

--- server/api/controllers/blockchain.js
+++ server/api/controllers/blockchain.js
@@ -1,41 +1,46 @@
-const Web3 = require('web3')
-const MyConc = require('../../../blockchain/build/contracts/KYC.json')
-const adminAddress = process.env.ADMIN_ADDRESS
-const adminKey = process.env.ADMIN_KEY
+const Web3 = require("web3");
+const MyConc = require("../../../blockchain/build/contracts/KYC.json");
+const adminAddress = process.env.ADMIN_ADDRESS;
+const adminKey = process.env.ADMIN_KEY;
 
-module.exports.getDetails = async(kycId)=>{
-    const web3 = new Web3("http://localhost:7545")
-    const netId = await web3.eth.net.getId()
-    const conc = new web3.eth.Contract(
-        MyConc.abi,
-        MyConc.networks[netId].address
-    )
-    const data = await conc.methods.getCustomerDetails(kycId).call({from:adminAddress})
-    return data;
-}
+module.exports.getDetails = async (kycId) => {
+  const web3 = new Web3("http://localhost:7545");
+  const netId = await web3.eth.net.getId();
+  const conc = new web3.eth.Contract(MyConc.abi, MyConc.networks[netId].address);
+  const data = await conc.methods.getCustomerDetails(kycId).call({ from: adminAddress });
+  return data;
+};
 
-module.exports.handelRequest = async(kycId,bAddress,response)=>{
-    const web3 = new Web3("http://localhost:7545")
-    const netId = await web3.eth.net.getId()
-    const conc = new web3.eth.Contract(
-        MyConc.abi,
-        MyConc.networks[netId].address
-    )
-    const tx = conc.methods.manageRequest(kycId, bAddress, response)
-    const gas = await tx.estimateGas({from:adminAddress})
-    const gasPrice = await web3.eth.getGasPrice();
-    const data = tx.encodeABI();
-    const nonce = await web3.eth.getTransactionCount(adminAddress)
+module.exports.getReqList = async (kycId) => {
+  const web3 = new Web3("http://localhost:7545");
+  const netId = await web3.eth.net.getId();
+  const conc = new web3.eth.Contract(MyConc.abi, MyConc.networks[netId].address);
+  const data = await conc.methods.getClientData(kycId).call({ from: adminAddress });
+  return data;
+};
 
-    const signedTx = await web3.eth.accounts.signTransaction({
-        to: conc.options.address,
-        data,
-        gas,
-        gasPrice,
-        nonce,
-        chainId: netId
-    },adminKey)
+module.exports.handelRequest = async (kycId, bAddress, response) => {
+  const web3 = new Web3("http://localhost:7545");
+  const netId = await web3.eth.net.getId();
+  const conc = new web3.eth.Contract(MyConc.abi, MyConc.networks[netId].address);
+  const tx = conc.methods.manageRequest(kycId, bAddress, response);
+  const gas = await tx.estimateGas({ from: adminAddress });
+  const gasPrice = await web3.eth.getGasPrice();
+  const data = tx.encodeABI();
+  const nonce = await web3.eth.getTransactionCount(adminAddress);
 
-    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-    return receipt;
-}
+  const signedTx = await web3.eth.accounts.signTransaction(
+    {
+      to: conc.options.address,
+      data,
+      gas,
+      gasPrice,
+      nonce,
+      chainId: netId,
+    },
+    adminKey
+  );
+
+  const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+  return receipt;
+};
--- server/api/routes.js
+++ server/api/routes.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const { notFound,home } = require("./controllers/home");
-const { register, login, getClientData, request } = require("./controllers/userController");
+const passport = require("passport");
+const { notFound, home } = require("./controllers/home");
+const {
+  register,
+  login,
+  getClientData,
+  getBankList,
+  request,
+} = require("./controllers/userController");
 
+router.get("/", home);
+router.post("/register", register);
+router.post("/login", login);
+router.get(
+  "/getClientData",
+  passport.authenticate("jwt", { session: false }),
+  getClientData
+);
+router.get("/getBankList", passport.authenticate("jwt", { session: false }), getBankList);
+router.post("/request", passport.authenticate("jwt", { session: false }), request);
 
-router.get('/', home)
-router.post('/register',register)
-router.post('/login',login)
-router.get('/getClientData', passport.authenticate('jwt', { session:false }), getClientData)
-router.post('/request', passport.authenticate('jwt', { session:false }), request)
-
-
-router.get('*', notFound)
-
+router.get("*", notFound);
 
 module.exports = router;
