78c40a39dd9277366154f294fe5ec8860530349c
Ayush Yadav <ayushyadav935@gmail.com>
2022-01-21 23:31:30 +0530

    Video server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const server = require("http").createServer(app);
 const cors = require('cors');
 const logger = require('./logger');
 const passport = require('passport');
@@ -8,6 +9,7 @@ require('dotenv').config();
 require('./config/db');
 require('./config/passJWT');
 
+
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
@@ -15,6 +17,48 @@ app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
 app.use('/', require('./api/routes'));
+
+
+//Video Calling Server
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id);
+  
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+      io.to(userToCall).emit("callUser", {
+        signal: signalData,
+        from,
+        name,
+      });
+    });
+  
+    socket.on("updateMyMedia", ({ type, currentMediaStatus }) => {
+      console.log("updateMyMedia");
+      socket.broadcast.emit("updateUserMedia", { type, currentMediaStatus });
+    });
+  
+    socket.on("msgUser", ({ name, to, msg, sender }) => {
+      io.to(to).emit("msgRcv", { name, msg, sender });
+    });
+  
+    socket.on("answerCall", (data) => {
+      socket.broadcast.emit("updateUserMedia", {
+        type: data.type,
+        currentMediaStatus: data.myMediaStatus,
+      });
+      io.to(data.to).emit("callAccepted", data);
+    });
+    socket.on("endCall", ({ id }) => {
+      io.to(id).emit("endCall");
+    });
+});
+
 
 
 
